18616a74072f9df2ef5633174621ff746705c2b9
Manuel Puyol <manuelpuyol@github.com>
2021-02-16 13:58:46 -0600

    working state

---
+++ .storybook/main.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app"
+  ],
+  "refs": {
+    "view-components": {
+      "title": "ViewComponents",
+      "url": "https://primer-view-components.herokuapp.com"
+    },
+    "react-components": {
+      "title": "React Components",
+      "url": "http://localhost:6006"
+    }
+  }
+}
